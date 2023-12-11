fdef1b2461b0d192fccb44c3601257d1164006a9
Snegan29 <pssj2914@gmail.com>
2023-12-11 11:29:06 +0530

    Task done

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,13 +1,63 @@
 import React, { Component } from "react";
 import "./Carousel.css";
-import { images } from "../data/CarouselData";
-// you can explore more - and check as how to use materiul ui
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import { images } from "../data/CarouselData";
 
-// implement the class below
 class Carousel extends Component {
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentImageIndex: 0,
+    };
+  }
+
+  switchImage = (direction) => {
+    this.setState((prevState) => {
+      let newIndex;
+
+      if (direction === "forward") {
+        newIndex = (prevState.currentImageIndex + 1) % images.length;
+      } else {
+        newIndex =
+          prevState.currentImageIndex === 0
+            ? images.length - 1
+            : prevState.currentImageIndex - 1;
+      }
+
+      return { currentImageIndex: newIndex };
+    });
+  };
+
+  render() {
+    const { currentImageIndex } = this.state;
+
+    return (
+      <div className="carousel-container">
+        
+
+        <div className="carousel-content">
+          <img
+            src={images[currentImageIndex].img}
+            alt={images[currentImageIndex].title}
+            className="carousel-image"
+          />
+          <div className="carousel-text">
+            <h2>{images[currentImageIndex].title}</h2>
+            <p>{images[currentImageIndex].subtitle}</p>
+          </div>
+        </div>
+        <ArrowBackIosIcon
+          className="carousel-arrow"
+          onClick={() => this.switchImage("backward")}
+        />
+        <ArrowForwardIosIcon
+          className="carousel-arrow"
+          onClick={() => this.switchImage("forward")}
+        />
+      </div>
+    );
+  }
 }
 
 export default Carousel;
